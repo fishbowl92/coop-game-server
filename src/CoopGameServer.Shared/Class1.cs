7b3c076b2df269e6e0f7408a20148bf5542babe7
주민우 <wnalsdn92@naver.com>
2026-07-22 15:00:26 +0900

    refactor: oragnize solution into api shared and test projects

---
+++ src/CoopGameServer.Shared/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace CoopGameServer.Shared;
+
+public class Class1
+{
+
+}
---
+++ tests/CoopGameServer.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CoopGameServer.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
